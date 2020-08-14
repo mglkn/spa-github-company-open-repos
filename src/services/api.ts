571077338b2a6c90124a61439858e17e98418783
Valdimir Migalkin <thewumin@gmail.com>
2020-08-14 15:34:17 +0300

    added api (draft);

---
+++ src/services/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const REPOS_LIMIT = 10;
+
+export const fetchOrg = async (orgName: string) => {
+  try {
+    const { data } = await axios.get(`https://api.github.com/orgs/${orgName}`)
+    return data;
+  } catch (e) {
+    console.log(e.response);
+    if (e.response !== undefined && e.response.status === 404) {
+      throw new Error(`Organization ${orgName} not found`);
+    }
+
+    throw new Error('Network error. Check internet or reload page');
+  }
+}
+
+export const fetchRepos = async (orgName: string, page: number = 0) => {
+  try {
+    const { data } = await axios.get(`https://api.github.com/orgs/${orgName}/repos?page=${page}&per_page=${REPOS_LIMIT}`);
+    return data
+  } catch (e) {
+    if (e.response !== undefined && e.response.status === 404) {
+      throw new Error(`Organization ${orgName} not found`);
+    }
+
+    throw new Error('Network error. Check internet or reload page');
+  }
+}
